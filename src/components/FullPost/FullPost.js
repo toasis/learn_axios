79b4eb1f3e0a5c862e8e546d0be0eb7b465aad50
Viki Zhang <onlyzj@gmail.com>
2018-08-19 22:57:59 +0800

    delete a post from the fake server.

--- src/components/FullPost/FullPost.js
+++ src/components/FullPost/FullPost.js
@@ -6,6 +6,28 @@ class FullPost extends Component {
   state = {
     loadedPost: null
   };
+  render() {
+    let post = <p style={{ textAlign: "center" }}>Please select a Post!</p>;
+    if (this.props.id) {
+      post = <p style={{ textAlign: "center" }}>Loading!</p>;
+    }
+    if (this.state.loadedPost) {
+      post = (
+        <div className="FullPost">
+          <h1>{this.state.loadedPost.title}</h1>
+          <p>{this.state.loadedPost.body}</p>
+          <div className="Edit">
+            <button className="Delete" onClick={this.deletePostHandler}>
+              Delete
+            </button>
+          </div>
+        </div>
+      );
+    }
+    console.log("a post rendered");
+    return post;
+  }
+
   componentDidUpdate() {
     if (this.props.id)
       if (
@@ -22,25 +44,12 @@ class FullPost extends Component {
       `componentDidUpdate + this.state.loadedPost  ${this.state.loadedPost}`
     );
   }
-  render() {
-    let post = <p style={{ textAlign: "center" }}>Please select a Post!</p>;
-    if (this.props.id) {
-      post = <p style={{ textAlign: "center" }}>Loading!</p>;
-    }
-    if (this.state.loadedPost) {
-      post = (
-        <div className="FullPost">
-          <h1>{this.state.loadedPost.title}</h1>
-          <p>{this.state.loadedPost.body}</p>
-          <div className="Edit">
-            <button className="Delete">Delete</button>
-          </div>
-        </div>
-      );
-    }
-    console.log("a post rendered");
-    return post;
-  }
+  deletePostHandler = () => {
+    axios
+      .delete(`https://jsonplaceholder.typicode.com/posts/${this.props.id}`)
+      .then(response => {
+        console.log(response);
+      });
+  };
 }
-
 export default FullPost;
