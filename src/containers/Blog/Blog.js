60909717bad8ec86d9a99da41663209a9872ed2d
Viki Zhang <onlyzj@gmail.com>
2018-08-19 21:00:54 +0800

    transform Data from fake server to 4 posts.

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -4,16 +4,35 @@ import Post from "../../components/Post/Post";
 import FullPost from "../../components/FullPost/FullPost";
 import NewPost from "../../components/NewPost/NewPost";
 import "./Blog.css";
+import axios from "axios";
 
 class Blog extends Component {
+  state = {
+    posts: []
+  };
+  componentDidMount() {
+    console.log('component Mounted');
+    axios.get("https://jsonplaceholder.typicode.com/posts").then(response => {
+      const posts = response.data.slice(0, 4);
+      const updatedPosts = posts.map(post => {
+        return {
+          ...post,
+          author: "Max"
+        };
+      });
+      this.setState({ posts: updatedPosts });
+      console.log(updatedPosts);
+    });
+  }
   render() {
+    console.log('rendered!');
+    const posts = this.state.posts.map(post => {
+      console.log(post);
+      return <Post title={post.title} key={post.id} author={post.author} />;
+    });
     return (
       <div>
-        <section className="Posts">
-          <Post />
-          <Post />
-          <Post />
-        </section>
+        <section className="Posts">{posts}</section>
         <section>
           <FullPost />
         </section>
